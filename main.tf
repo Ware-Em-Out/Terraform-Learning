3334b1e5cfe67844725bb8e4b9341c256fc9bcff
Ware-Em-Out <DL_Ware@outlook.com>
2022-09-10 21:13:25 -0400

    Updated README and main.tf Added vNet configurations.

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = ">= 3.20.0"
     }
   }
@@ -12,34 +12,34 @@ terraform {
 provider "azurerm" {
   // Configuration options (i.e. Subscription ID, Tenant ID.)
   subscription_id = "1635b9ea-d00f-4800-aabc-c7dde1c396a3"
-  tenant_id = "8d2e9fbb-af54-4389-9cb3-d447058b1220"
-  client_id = "15c188b9-f156-418a-928a-71d31427808e"
-  client_secret = "N9B8Q~S-YKikdrkb3iVDsH_W4e-bB~Jy~l0dgafg"
+  tenant_id       = "8d2e9fbb-af54-4389-9cb3-d447058b1220"
+  client_id       = "15c188b9-f156-418a-928a-71d31427808e"
+  client_secret   = "N9B8Q~S-YKikdrkb3iVDsH_W4e-bB~Jy~l0dgafg"
   features {}
 }
 
 locals {
-  // Local vairables for our resources will be defined here.
-  resource_group_name = "Terri"
-  storage_account_name = "storeterri"
+  // Local variables for our resources will be defined here.
+  resource_group_name    = "Terri"
+  storage_account_name   = "storeterri"
   storage_container_name = "dataterri"
-  storage_blob_name = "main.tf" 
-  location = "East US"
+  storage_blob_name      = "terriblob"
+  location               = "East US"
   virtual_network = {
-    name = "terri-vnet"
-    address_space = ["10.0.0.0/16"]
-    }
-    subnets =[
-      {
-        name ="subnet1"
-        address_prefix="10.0.0.0/24"
-      },
-      {
-        name ="subnet2"
-        address_prefix="10.0.1.0/24"
-      }
-    ]
+    name          = "terri-vnet"
+    address_space = "10.0.0.0/16"
   }
+  subnets = [
+    {
+      name           = "subnet1"
+      address_prefix = "10.0.0.0/24"
+    },
+    {
+      name           = "subnet2"
+      address_prefix = "10.0.1.0/24"
+    }
+  ]
+}
 
 resource "azurerm_resource_group" "rg" {
   name     = local.resource_group_name
@@ -52,7 +52,7 @@ resource "azurerm_storage_account" "storage" {
   location                 = local.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  account_kind = "StorageV2"
+  account_kind             = "StorageV2"
 }
 
 resource "azurerm_storage_container" "container" {
@@ -71,16 +71,16 @@ resource "azurerm_storage_blob" "blob" {
 
 resource "azurerm_virtual_network" "vnet" {
   name                = local.virtual_network.name
-  location            = local.location 
+  location            = local.location
   resource_group_name = local.resource_group_name
-  address_space       = local.virtual_network.address_space
-  
+  address_space       = [local.virtual_network.address_space]
+
   subnet {
     name           = local.subnets[0].name
     address_prefix = local.subnets[0].address_prefix
   }
 
-   subnet {
+  subnet {
     name           = local.subnets[1].name
     address_prefix = local.subnets[1].address_prefix
   }
